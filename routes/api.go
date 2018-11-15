8dd98db9fba962c34948d8dae236b864ae24a36a
Elias Mawa <emawa@sawol.moe>
2018-11-15 02:29:08 -0700

    comment

--- routes/api.go
+++ routes/api.go
@@ -4,12 +4,14 @@ import (
 	"github.com/kataras/iris"
 )
 
-// APIController The controller for /api
+// APIController The controller for /a
 type APIController struct{}
 
-// Get serves
-// Method:   GET
-// Resource: http://localhost:8080
+/*
+Get serves
+Method:   GET
+Resource: http://localhost:8080
+*/
 func (c *APIController) Get(ctx iris.Context) {
 	ctx.View("index.pug")
 }
--- routes/root.go
+++ routes/root.go
@@ -1,16 +1,21 @@
+/*
+Package routes - Contains all of the top level routes
+*/
 package routes
 
 import (
 	"github.com/kataras/iris"
 )
 
-// ROOTController The controller for /api
+// ROOTController The controller for /
 type ROOTController struct {
 }
 
-// Get serves
-// Method:   GET
-// Resource: http://localhost:8080
+/*
+Get serves
+Method:   GET
+Resource: http://localhost:8080
+*/
 func (c *ROOTController) Get(ctx iris.Context) {
 
 	ctx.ViewData("title", "gopy")
