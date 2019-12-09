4a4388df553eec0e082ddcd1904b5998ae11538a
Elias Mawa <emawa@sawol.moe>
2019-12-08 22:41:14 -0700

    tweak

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
-if [[ $1 = "run" ]]
+if [ $1 = "run" ]
 then
 	echo "running..."
 
 	./dist/gopy
 
 	echo "done..."
-elif [[ $1 = "build" ]]
+elif [ $1 = "build" ]
 then
 	echo "building..."
 	
