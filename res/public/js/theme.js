340e42b34e158f6e7df0c72d72f5b11169137916
Elias <elias@emawa.io>
2023-01-18 14:18:08 -0700

    fix: update theme script

--- res/public/js/theme.js
+++ res/public/js/theme.js
@@ -3,9 +3,9 @@ $("document").ready(function () {
   // let light = "https://cdn.jsdelivr.net/npm/ashleycss@4.1.52/dist/themes/ashleycss-sleepy.min.css";
   let light = "https://cdn.jsdelivr.net/npm/ashleycss@4.1.52/dist/themes/ashleycss-sakura.min.css";
 
-  let theme = Cookies.get("theme");
+  const persisted_theme = Cookies.get("theme");
 
-  if (!theme || theme == "dark") {
+  if (!persisted_theme || persisted_theme === "dark") {
     $("#style").attr("href", dark);
     $("#theme").html("lights on!");
   } else {
@@ -14,8 +14,9 @@ $("document").ready(function () {
   }
 
   $("#theme").on("click", () => {
-    let theme = Cookies.get("theme");
-    if (theme == "dark") {
+    const current_theme = Cookies.get("theme");
+
+    if (current_theme == "dark") {
       Cookies.set("theme", "light", { expires: 365 });
       $("#style").attr("href", light);
       $("#theme").html("lights off!");
